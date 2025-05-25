8bb48a01f7872efcf81844732c30e532e81faaab
Siyavush1411 <siyavushmirzaev375@gmail.com>
2025-05-26 00:16:23 +0500

    added register page

--- src/main.js
+++ src/main.js
@@ -1,6 +1,29 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from '@/config/routings/'
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+import '@mdi/font/css/materialdesignicons.css'
 
-createApp(App).mount('#app')
+const vuetify = createVuetify({
+  components,
+  directives,
+  theme: {
+    defaultTheme: 'light',
+    themes: {
+      light: {
+        colors: {
+          primary: '#1867C0',
+          secondary: '#5CBBF6',
+        }
+      }
+    }
+  }
+})
+
+const app = createApp(App)
+app.use(router)
+app.use(vuetify)
+app.mount('#app')
