b8490755597bcd00de0b0d26dc734c1a7113d53a
Siyavush1411 <siyavushmirzaev375@gmail.com>
2025-05-26 23:49:10 +0500

    added new components and default page

---
+++ src/components/AppFooter.vue
@@ -0,0 +1,37 @@
+<template>
+  <v-footer class="bg-primary">
+    <v-container>
+      <v-row>
+        <v-col cols="12" md="4">
+          <h3 class="text-h5 text-white">Контакты</h3>
+          <p class="text-white mt-4">
+            Email: science@pittu.edu<br>
+            Телефон: +7 123 456-78-90
+          </p>
+        </v-col>
+        <v-col cols="12" md="4">
+          <h3 class="text-h5 text-white">Быстрые ссылки</h3>
+          <v-list class="bg-transparent">
+            <v-list-item
+              v-for="(link, i) in links"
+              :key="i"
+              :to="link.to"
+              class="text-white"
+            >
+              {{ link.title }}
+            </v-list-item>
+          </v-list>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-footer>
+</template>
+
+<script setup>
+defineProps({
+  links: {
+    type: Array,
+    required: true
+  }
+})
+</script>
---
+++ src/components/RecentWorksSection.vue
@@ -0,0 +1,37 @@
+<template>
+  <v-container class="my-15">
+    <h2 class="text-h3 text-center mb-10">Последние загрузки</h2>
+    <v-row>
+      <v-col v-for="(work, i) in recentWorks" :key="i" cols="12" md="4">
+        <v-card class="pa-4">
+          <v-card-title class="text-h5">
+            {{ work.title }}
+          </v-card-title>
+          <v-card-subtitle>
+            {{ work.author }} • {{ work.date }}
+          </v-card-subtitle>
+          <v-card-text>
+            <v-chip
+              v-for="(tag, j) in work.tags"
+              :key="j"
+              class="ma-1"
+              color="primary"
+              variant="outlined"
+            >
+              {{ tag }}
+            </v-chip>
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script setup>
+defineProps({
+  recentWorks: {
+    type: Array,
+    required: true
+  }
+})
+</script>
